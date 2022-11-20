897109f8364c68f411b383f3cacad9278ea68026
chihsuanwu <chihsuanw@gmail.com>
2022-11-20 17:30:09 +0800

    Add unit test workflow

--- tests/test_api_get.py
+++ tests/test_api_get.py
@@ -1,7 +1,9 @@
-import sys
-sys.path.append('../')
+import os, sys
+ABS_PATH = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(ABS_PATH, os.pardir))
 
 from src.tdx_proxy import TDXProxy
+
 
 def __fetch_basic(proxy: TDXProxy):
     params = {
@@ -10,17 +12,14 @@ def __fetch_basic(proxy: TDXProxy):
     }
     return proxy.get('v2/Bus/Alert/City/Taichung', params=params)
 
+
 def test_from_credentials_file():
-    proxy = TDXProxy.from_credential_file("credential.json")
+    proxy = TDXProxy.from_credential_file("tests/credential.json")
     result = __fetch_basic(proxy)
     assert result.status_code == 200
+
 
 def test_no_auth():
     proxy = TDXProxy.no_auth()
     result = __fetch_basic(proxy)
     assert result.status_code == 200
-
-
-if __name__ == '__main__':
-    test_from_credentials_file()
-    test_no_auth()
